080f77ba999b85562a773f6049725d367d066a1d
Bhumi Patel <bhumip214@gmail.com>
2019-05-31 10:38:54 -0700

    refactor again and added fullpath to stat (now done with MVP)

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,14 +2,35 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-
+#include <unistd.h>
+#include <string.h>
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+
+  DIR *d;
+  char *path;
+
+  // Parse command line to find directory
+  if (argc == 1)
+  {
+    path = ".";
+    printf("Directory: %s\n", path);
+  }
+  else if (argc == 2)
+  {
+    path = argv[1];
+    printf("Directory: %s\n", path);
+  }
+  else
+  {
+    printf("Cannot find directory\n");
+    exit(1);
+  }
   // Open directory
-  DIR *d = opendir(".");
+  d = opendir(path);
   // checking for errors
   if (d == NULL)
   {
@@ -22,7 +43,12 @@ int main(int argc, char **argv)
   {
     //printf("File name:%s\n", ent->d_name);
     struct stat buf;
-    stat(ent->d_name, &buf);
+    char fullpath[strlen(path) + strlen(ent->d_name)];
+    strcpy(fullpath, path);
+    strcat(fullpath, "/");
+    strcat(fullpath, ent->d_name);
+
+    stat(fullpath, &buf);
     //printf("File size: %lld bytes\n", buf.st_size);
     printf("%lld %s\n", buf.st_size, ent->d_name);
   }
